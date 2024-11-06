b0281b2e27a8a4ec7755c0cdda2a0f7992ce1dec
yoonjungeun1 <jeyoon.dev@hustation.com>
2024-11-06 10:59:35 +0900

    feat: 동일 네트워크 환경으로 변경

--- src/consumer.py
+++ src/consumer.py
@@ -4,8 +4,8 @@ import json
 async def async_kafka_consumer(topic: str, group_id: str) -> AIOKafkaConsumer:
     consumer = AIOKafkaConsumer(
         topic,
-        # bootstrap_servers=['192.168.10.26:9094'],
-        bootstrap_servers=['kafka-broker.stockly.svc.cluster.local:9092'],
+        bootstrap_servers=['kafka:9092'],
+        # bootstrap_servers=['kafka-broker.stockly.svc.cluster.local:9092'],
         group_id=group_id,
         auto_offset_reset='latest',
         enable_auto_commit=True,
