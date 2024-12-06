9231b8f5eb5dfdec2be29a7a6fdf72842af31362
yoonjungeun1 <jeyoon.dev@hustation.com>
2024-12-06 13:37:31 +0900

    feat: 프론트 주소 추가

--- src/main.py
+++ src/main.py
@@ -343,8 +343,7 @@ app.include_router(alert_routes.router)
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["http://localhost:5173"],
-    allow_origins=["https://stockly-frontend.vercel.app/"],
+    allow_origins=["https://stockly-frontend.vercel.app", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
